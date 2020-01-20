46ba7c0a3a67e0fbb1790bc954e81c2ba80a344d
RyuCoder <rockstar.ryucoder@gmail.com>
2020-01-20 16:49:02 +0530

    Custom Endpoints Are Being Documented Correctly.

--- book/serializers.py
+++ book/serializers.py
@@ -15,3 +15,12 @@ class BookCustomListSerializer(serializers.ModelSerializer):
     class Meta: 
         model = Book
         fields = ["id", "name"]
+
+
+class BookCustomDetailSerializer(serializers.ModelSerializer):
+
+    is_premium = serializers.BooleanField(default=False)
+    
+    class Meta: 
+        model = Book
+        fields = ["id", "name", "is_premium"]
--- book/views.py
+++ book/views.py
@@ -1,24 +1,38 @@
+from pprint import pprint
+
 from django.shortcuts import render
 
 from rest_framework import viewsets
 from rest_framework.decorators import action
 from rest_framework.response import Response
 from rest_framework import status
+from rest_framework.schemas.openapi import AutoSchema
 
-from book.serializers import BookSerializer, BookCustomListSerializer
+from book.serializers import BookSerializer, BookCustomListSerializer, BookCustomDetailSerializer
 from book.models import Book
 
 
+class CustomSchema(AutoSchema):
+    def get_operation(self, *args, **kwargs):
+        # Implement custom introspection here (or in other sub-methods)
+
+        operation = super(CustomSchema, self).get_operation(*args, **kwargs)
+
+        # modify the operation he for fine grain control over what you want to show in docs
+
+        return operation  
+        
 
 class BookViewSet(viewsets.ModelViewSet):
     serializer_class = BookSerializer
     queryset = Book.objects.all()
+    schema = CustomSchema()
 
-    
-    @action(detail=False, methods=['get'])
+
+    @action(detail=False, methods=['get'], serializer_class=BookCustomListSerializer)
     def custom_list(self, request, pk=None):
         
-        serializer = BookCustomListSerializer()
+        # serializer = BookCustomListSerializer()
         print()
         print()
         print("Damn! I was printed!!")
@@ -27,9 +41,10 @@ class BookViewSet(viewsets.ModelViewSet):
 
         return Response(serializer.errors, status=status.HTTP_200_OK)
 
-    @action(detail=True, methods=['post'])
+    @action(detail=True, methods=['post'], serializer_class=BookCustomDetailSerializer)
     def custom_detail(self, request, pk=None):
         
+        # instance = BookCustomDetailSerializer.objects.get()
         print()
         print()
         print("Damn! I was printed!!")
@@ -37,3 +52,11 @@ class BookViewSet(viewsets.ModelViewSet):
         print()
 
         return Response(serializer.errors, status=status.HTTP_200_OK)
+    
+    
+    # Pass schema=None into the action decorator to disaable the documentation of a particular endpoint.
+    # @action(detail=True, methods=['post'], schema=None)
+    # def custom_detail(self, request, pk=None):
+    #     return Response(serializer.errors, status=status.HTTP_200_OK)
+
+
